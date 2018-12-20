2a848b6fdf17ccea811717607a8b7568083d2cb3
Irham Rizady <rirham12@gmail.com>
2018-12-20 19:34:41 +0700

    fix some source code

--- src/finalproject/connection.java
+++ src/finalproject/connection.java
@@ -4,29 +4,25 @@
  * and open the template in the editor.
  */
 package finalproject;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
+import java.sql.*;
 /**
  *
  * @author Irham Rizady
  */
 public class connection {
-    public static Connection conn;
-    public static Statement stm;
-    public static void main(String[] args) {
-        try{
-            String url = "jdbc:mysql://localhost/servis";
-            String user = "root";
-            String pass = "";
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(url, user, pass);
-            stm = conn.createStatement();
-            System.out.println("Koneksi Berhasil");
-        } catch(Exception e){
-            System.err.println("Koneksi Gagal" +e.getMessage());
+    private static Connection conn;
+    public static Connection getConn(){
+        if(conn == null){
+            try {
+                String url = "jdbc:mysql://localhost/servis";
+                String user = "root";
+                String pass = "";
+                DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+                conn = DriverManager.getConnection(url, user, pass);
+            } catch (Exception e) {
+                System.out.println("Gagal Koneksi Database");
+            }
         }
+        return conn;
     }
 }
